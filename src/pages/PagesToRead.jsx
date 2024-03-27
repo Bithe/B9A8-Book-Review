bc6284dec6494becc66737c2e579ef90e6e4583d
Bithe <farzana.hossain5@gmail.com>
2024-03-27 17:03:31 +0600

    redirects done

--- src/pages/PagesToRead.jsx
+++ src/pages/PagesToRead.jsx
@@ -1,20 +1,71 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from "recharts";
 
 const PagesToRead = () => {
+  const [readBooks, setReadBooks] = useState([]);
 
-    const [readBooks, setReadBooks] = useState([]);
+  useEffect(() => {
+    axios
+      .get("booksData.json")
+      // .then(data => console.log(data.data));
+      .then((data) => {
+        const bookData = data.data;
+        const readBooksData = bookData.map((book) => {
+          const obj = {
+            name: book.bookName,
+            pages: book.totalPages,
+          };
+          return obj;
+        });
+        console.log(readBooksData);
+        setReadBooks(readBooksData);
+      });
+  }, []);
 
-    useEffect(()=>{
-        axios.get('booksData.json')
-        .then(data => console.log(data.data));
-    },[])
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
 
-    return (
-        <div>
-            <h1>Chart</h1>
-        </div>
-    );
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  return (
+    <div className="flex items-center justify-center ">
+      <h1>Chart</h1>
+      <BarChart
+        width={500}
+        height={300}
+        data={readBooks}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar dataKey="pages" fill="#8884d8" shape={<TriangleBar />} label={{ position: "top" }}>
+          {readBooks.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </div>
+  );
 };
 
 export default PagesToRead;
